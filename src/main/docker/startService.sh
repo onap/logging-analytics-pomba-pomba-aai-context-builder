cd0fcf3be1ee7548f5d17cfb4867b421471cad5e
Leigh, Phillip (pl876u) <phillip.leigh@amdocs.com>
2018-11-02 19:12:22 -0400

    BugFix:CtxAggr not picking up posted events
    Issue-ID: LOG-794
    
    Change-Id: I24d6f283e6101e7928d353252e470f552a528480
    Signed-off-by: Leigh, Phillip (pl876u) <phillip.leigh@amdocs.com>

--- src/main/docker/startService.sh
+++ src/main/docker/startService.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 cd /opt/app
+preStartFile="/opt/app/bin/pre_start.sh"
+if [ -f "$preStartFile" ]
+then
+	echo "$preStartFile found."
+        exec $preStartFile
+else
+	echo "$preStartFile not found."
+fi
+
+cd /opt/app
 if [ -z "${java_runtime_arguments}" ]; then
     java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/pomba-aai-context-builder.jar
 else
